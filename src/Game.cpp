cab57897ff9dc63ca82777320bf995d7f8d2550b
chris harn <chris.harn@gmail.com>
2018-10-19 14:47:26 -0700

    Added Game Class and Game interface

---
+++ src/Game.cpp
@@ -0,0 +1,29 @@
+#include "Game.h"
+
+Game::Game() {
+
+}
+
+Game::~Game() {
+
+}
+
+void Game::init() {
+
+}
+
+void Game::render() {
+
+}
+
+void Game::update() {
+
+}
+
+void Game::handleEvents() {
+
+}
+
+void clean() {
+
+}
---
+++ src/Game.h
@@ -0,0 +1,18 @@
+#ifndef __GAME__
+#define __GAME__
+
+class Game {
+public:
+	void init();
+	void render();
+	void update()
+	void handleEvents()
+	void clean();
+	
+	Game();
+	~Game();
+private:
+	bool g_bRunning;
+};
+
+#endif
