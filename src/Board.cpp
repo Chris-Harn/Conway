0bebb05763dffaf8144d30856041f0bd623f33a1
chris harn <chris.harn@gmail.com>
2018-10-20 08:21:52 -0700

    Removed Array from Board

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,11 +1,14 @@
 #include "Board.h"
 
-Board::Board( int width, int height ) {
-	m_tableWidth = width;
-	m_tableHeight = height;
-	m_pTable = 0;
+#include "Tile.h"
+
+Board::Board( int width, int height ) : m_tableWidth( width ), m_tableHeight( height ), m_pTable( 0 ) {
+
 }
 
 Board::~Board() {
-
+	if( m_pTable != 0 ) {
+		delete m_pTable;
+		m_pTable = 0;
+	}
 }
