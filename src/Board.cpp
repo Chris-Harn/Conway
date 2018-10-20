75cb1fe58345c7cdd9b17e144b86203b2883a31c
chris harn <chris.harn@gmail.com>
2018-10-20 09:41:44 -0700

    Creates tiles for board.

--- src/Board.cpp
+++ src/Board.cpp
@@ -6,19 +6,25 @@
 
 Board::Board( int width, int height ) : m_tableWidth( width ), m_tableHeight( height ), m_pTable( 0 ) {
 	m_pTable = new Tile[ width * height ];
+	
+	int currentNumber;	
+	for( int j = 0; j < m_tableHeight; j++ ) {
+			for( int i = 0; i < m_tableWidth; i++ ) {
+				currentNumber = i + ( j * m_tableWidth );
+				m_pTable[ currentNumber ].setX( i + 1 );
+				m_pTable[ currentNumber ].setY( j + 1 );
+			}
+		}
 }
 
 Board::~Board() {
 	if( m_pTable != 0 ) {
 		for( int j = 0; j < m_tableHeight; j++ ) {
 			for( int i = 0; i < m_tableWidth; i++ ) {
-				std::cout << " " << i + ( j * m_tableWidth );
-				delete *m_pTable[ i + ( j * m_tableWidth ) ];
-				m_pTable[ i + ( j * m_tableWidth ) ] = 0;
+				// std::cout << " " << i + ( j * m_tableWidth );
+				//delete *m_pTable[ i + ( j * m_tableWidth ) ];
+				// m_pTable[ i + ( j * m_tableWidth ) ] = 0;
 			}
-			std::cout << "\n";
 		}
-
-		// m_pTable = 0;
 	}
 }
