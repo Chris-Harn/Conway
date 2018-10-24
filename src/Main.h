3094c234fdc9c64dc0eee9792a85b1909a2aeb74
chris harn <chris.harn@gmail.com>
2018-10-24 14:11:18 -0700

    Updated Main.h variables

--- src/Main.h
+++ src/Main.h
@@ -1,15 +1,19 @@
 #ifndef __MAIN__
 #define __MAIN__
 
+// Gameplay Loop
 const double FPS = 15;
 const double DELAY_RENDERER = 1000 / FPS;
 const double DELAY_UPDATE_BOARD = 1000 / 5;
+
+// Board Settings
 const int GAME_WIDTH = 80;
 const int GAME_HEIGHT = 40;
+
+// SDL Settings
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
-
-// window context for graphics
-const char windowContext[] = "Console";  // "SDL"
+const int CELL_SIZE_X = 2;
+const int CELL_SIZE_Y = 2;
 
 #endif
