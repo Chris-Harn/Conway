87cb193991405d03a4388466e0ad7e2942e0f4f9
chris harn <chris.harn@gmail.com>
2018-11-11 20:13:58 -0800

    Added function definitions to Message.cpp

--- src/Message.cpp
+++ src/Message.cpp
@@ -2,9 +2,29 @@
 
 #include <iostream>
 
+
+/**************************************************
+ *  message
+ *  Purpose: Print string literal that is passed in
+ *  to console. 
+ *  Input: const char * from string literal.
+ *  Return: None.
+ *
+ *************************************************/
+
 void messsage( const char *message ) {
 	std::cout << "  " << message << " \n";
 }
+
+
+/**************************************************
+ *  errorMessage 
+ *  Purpose: Print string literal that is passed in
+ *  to console as error message.
+ *  Input: const char * from string literal.
+ *  Return: None.
+ *
+ *************************************************/
 
 void errorMessage( const char *errorMessage ) {
 	std::cout << "** Error: " << errorMessage << " **\n";
