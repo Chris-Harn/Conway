e271088d50637b4cd79c21f5c7fd68804446a167
chris harn <chris.harn@gmail.com>
2018-11-11 20:24:08 -0800

    Added function definitions for Tile.cpp

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -2,28 +2,47 @@
 
 #include "Main.h"
 
+
+/**************************************************
+ *  Tile::Tile
+ *  Purpose: Constructor. Pauses tile to dead and 
+ *  zero live neighbors.
+ *  Input: None.
+ *  Return: None.
+ *
+ *************************************************/
+
 Tile::Tile() : m_balive( 0 ), m_numberOfLiveNeighbors( 0 ) {
 
 }
+
+
+/**************************************************
+ *  Tile::~Tile
+ *  Purpose: Deconstructor
+ *  Input: None.
+ *  Return: None.
+ *
+ *************************************************/
 
 Tile::~Tile() {
 
 }
 
+
+/**************************************************
+ *  Tile::findIfChanging
+ *  Purpose: Decides new state of the tile, compares
+ *  it to old state, and returns true if states are
+ *  different.
+ *  Input: None.
+ *  Return: bool (true) if the changing dead/alive
+ *  status
+ *
+ *************************************************/
+
 bool Tile::findIfChanging() {
 	bool newStatus = m_balive;
-
-	/*
-	if ( m_numberOfLiveNeighbors < 2 ) {
-		newStatus = false;
-	} else if ( m_numberOfLiveNeighbors == 3 ) {
-		newStatus = true;
-	} else if ( m_numberOfLiveNeighbors < 4 ) {
-		// do nothing
-	} else if ( m_numberOfLiveNeighbors > 3 ) {
-		newStatus = false;
-	}
-	*/
 
 	if( m_numberOfLiveNeighbors == REPRODUCE_NUMBER ) {
 		newStatus = true;
@@ -38,22 +57,69 @@ bool Tile::findIfChanging() {
 	return false;
 }
 
+
+/**************************************************
+ *  Tile::flip
+ *  Purpose: Changes the tile's state.
+ *  Input: None.
+ *  Return: None.
+ *
+ *************************************************/
+
 void Tile::flip() {
 	m_balive = !m_balive;
 }
 
+
+/**************************************************
+ *  Tile::alive
+ *  Purpose: Returns if the tile is alive or dead.
+ *  Input: None.
+ *  Return: None.
+ *
+ *************************************************/
+
 int Tile::alive() {
 	return m_balive;
 }
+
+
+/**************************************************
+ *  Tile::setLive
+ *  Purpose: Turns the tile living.
+ *  Input: None.
+ *  Return: None.
+ *
+ *************************************************/
 
 void Tile::setLive() {
 	m_balive = 1;
 	m_numberOfLiveNeighbors = 3;
 }
 
+
+/**************************************************
+ *  Tile::setLive
+ *  Purpose: Takes an int and sets that to the number
+ *  of living neighbors.
+ *  Input: int that holds number of living neighbors.
+ *  Return: None.
+ *
+ *************************************************/
+
 void Tile::setLive( int liveNeighbors ) {
 	m_numberOfLiveNeighbors = liveNeighbors;
 }
+
+
+/**************************************************
+ *  Tile::numberAlive
+ *  Purpose: Returns the number of neighbors alive
+ *  known to the cell. 
+ *  Input: None.
+ *  Return: int value of living neighbor tiles.
+ *
+ *************************************************/
 
 int Tile::numberAlive() {
 	return m_numberOfLiveNeighbors;
