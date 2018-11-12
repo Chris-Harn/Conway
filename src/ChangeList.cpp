cbeca986f148e72dd3ed47f9ca194884e3858b42
chris harn <chris.harn@gmail.com>
2018-11-11 20:08:03 -0800

    Added function definitions to ChangeList.cpp

--- src/ChangeList.cpp
+++ src/ChangeList.cpp
@@ -1,5 +1,16 @@
 #include "ChangeList.h"
 
+
+/**************************************************
+ *  ChangeList::ChangeList
+ *  Purpose: Constructor that is set to the size of
+ *  the board... allowing all spaces to be set to 
+ *  alive. 
+ *  Input: int that is sized to the board.
+ *  Return: None.
+ *
+ *************************************************/
+
 ChangeList::ChangeList( int sizeOfArray ) {
 	m_pcurrentList = 0;
 	m_pcurrentList = new int[ sizeOfArray ];
@@ -8,12 +19,32 @@ ChangeList::ChangeList( int sizeOfArray ) {
 	m_maxMemory = sizeOfArray;
 }
 
+
+/**************************************************
+ *  ChangeList::~ChangeList
+ *  Purpose: Deconstructor that deletes the memory 
+ *  allocated on the heap.
+ *  Input: None.
+ *  Return: None.
+ *
+ *************************************************/
+
 ChangeList::~ChangeList() {
 	if( m_pcurrentList != 0 ) {
 		delete []m_pcurrentList;
 		m_pcurrentList = 0;
 	}
 }
+
+
+/**************************************************
+ *  ChangeList::addNumberToList
+ *  Purpose: Adds a tile number (int) to the top of
+ *  the stack. First In, Last Out.
+ *  Input: int number to add to top of stack.
+ *  Return: None.
+ *
+ *************************************************/
 	
 void ChangeList::addNumberToList( int number ) {
 	if( m_totalNumbersInList < m_maxMemory ) {
@@ -23,6 +54,18 @@ void ChangeList::addNumberToList( int number ) {
 		// produce SDL error
 	}
 }
+
+
+/**************************************************
+ *  ChangeList::removeNumberFromList
+ *  Purpose: Pops last value off the stack. Value is
+ *  done by passing by reference.
+ *  Input: int num that is empty.
+ *  Return: bool (true) if the value returned is popped
+ *  off the top of the stack. bool (false) if no more
+ *  numbers to pop off.
+ *
+ *************************************************/
 
 bool ChangeList::removeNumberFromList( int &number ) {
 	if( m_totalNumbersInList > 0 ) {
